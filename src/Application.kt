1d3511cd99b58c01c3d86d7634d757d1f9a60c72
MarkNjunge <mark.kamau@outlook.com>
2019-02-14 12:39:18 +0300

    Initial commit

---
+++ src/Application.kt
@@ -0,0 +1,47 @@
+package com.marknjunge
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.features.*
+import org.slf4j.event.*
+import io.ktor.gson.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+fun Application.module() {
+    install(Compression) {
+        gzip {
+            priority = 1.0
+        }
+        deflate {
+            priority = 10.0
+            minimumSize(1024) // condition
+        }
+    }
+
+    install(StatusPages) {
+        exception<Throwable> { cause ->
+            log.error(cause.message)
+            call.respond(HttpStatusCode.InternalServerError)
+        }
+    }
+
+    install(CallLogging) {
+        level = Level.INFO
+        filter { call -> call.request.path().startsWith("/") }
+    }
+
+    install(ContentNegotiation) {
+        gson {}
+    }
+
+    routing {
+        get("/") {
+            call.respondText("GlobalGym API", contentType = ContentType.Text.Plain)
+        }
+    }
+}
