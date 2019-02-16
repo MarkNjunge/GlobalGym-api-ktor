3857812d89d8d21056618d5072ae980ec4c3a4f5
MarkNjunge <mark.kamau@outlook.com>
2019-02-16 15:19:56 +0300

    And environment variables for database

--- src/Config.kt
+++ src/Config.kt
@@ -6,9 +6,9 @@ class Config(application: Application) {
     val database = Database(application)
 
     class Database(application: Application) {
-        val url = application.configVar("database.url")
-        val username = application.configVar("database.username")
-        val password = application.configVar("database.password")
+        val url = System.getenv("DATABASE_URL") ?: application.configVar("database.url")
+        val username = System.getenv("DATABASE_USERNAME") ?: application.configVar("database.username")
+        val password = System.getenv("DATABASE_PASSWORD") ?: application.configVar("database.password")
     }
 }
 
